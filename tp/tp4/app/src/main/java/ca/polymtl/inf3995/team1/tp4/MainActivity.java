17c76a20b7b4e6b2430adbd1f25869cf674125e9
Emir Khaled Belhaddad <emir-khaled.belhaddad@polymtl.ca>
2018-09-11 12:25:24 -0400

    Created android project for TP4

---
+++ tp/tp4/app/src/main/java/ca/polymtl/inf3995/team1/tp4/MainActivity.java
@@ -0,0 +1,13 @@
+package ca.polymtl.inf3995.team1.tp4;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
