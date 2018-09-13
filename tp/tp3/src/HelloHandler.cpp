8ba875b9f91a7bceacdd9f5be3c8bc541ce7244e
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-09-13 15:28:01 -0400

    Made the test1, test2, test3 and 404 resources of the server.

--- tp/tp3/src/HelloHandler.cpp
+++ tp/tp3/src/HelloHandler.cpp
@@ -1,19 +1,32 @@
 #include "HelloHandler.hpp"
 
+#include <regex>
+
 #include "pistache/endpoint.h"
 
 void HelloHandler::onRequest(const Pistache::Http::Request& request, Pistache::Http::ResponseWriter response) {
     std::cout << "ressource \"" << request.resource() << '"' << std::endl;
 
     if (request.method() == Pistache::Http::Method::Get) {
-        if (request.resource() == "/test1") {
+        if (std::regex_match(request.resource(), std::regex("/test1/?"))) {
             response.send(Pistache::Http::Code::Ok, "test 1 est bon...\n");
         }
-        else if (request.resource() == "/test2") {
-            response.send(Pistache::Http::Code::Ok, "test 2 est bon...\n");
+        else if (std::regex_match(request.resource(), std::regex("/test2/?"))) {
+            response.send(Pistache::Http::Code::Ok, "<a href=\"http://www.polymtl.ca\">Poly Mtl</a>\n");
         }
-        else if (request.resource() == "/test3") {
-            response.send(Pistache::Http::Code::Ok, "test 3 est bon...\n");
+        else if (std::regex_match(request.resource(), std::regex("/test3/?"))) {
+            response.send(
+                Pistache::Http::Code::Ok,
+                "<!DOCTYPE html>\n"
+                "<html>\n"
+                "    <body>\n"
+                "        <img "
+                "          src=\"https://i.kym-cdn.com/photos/images/original/000/863/220/fbb.jpg\" "
+                "          alt=\"This should have been a good joke about Chuck Norris, but Chuck Norris didn't want us to joke about him.\""
+                "        />\n"
+                "    </body>\n"
+                "</html>\n"
+            );
         }
         else {
             send404(response);
@@ -25,5 +38,14 @@ void HelloHandler::onRequest(const Pistache::Http::Request& request, Pistache::H
 }
 
 void HelloHandler::send404(Pistache::Http::ResponseWriter& response) const {
-    response.send(Pistache::Http::Code::Not_Found, "404 Not found\n");
+    response.send(
+        Pistache::Http::Code::Not_Found,
+        "<!DOCTYPE html>\n"
+        "<html>\n"
+        "    <body>\n"
+        "        <div><p>404. Sorry, that's an error.<br/><h2>Sucks to be you</h2></p></div>\n"
+        "        <img src=\"https://cdn-images-1.medium.com/max/1600/0*AnVCpSvrAeldg3Rn.\"/>\n"
+        "    </body>\n"
+        "</html>\n"
+    );
 }
--- tp/tp3/src/main.cpp
+++ tp/tp3/src/main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <stdexcept>
+#include <inttypes.h>
+#include <sstream>
 
 #include "HelloHandler.hpp"
 
-int main() {
+int main(int argc, char** argv) {
+    uint32_t portId;
+    if (argc == 1) {
+        portId = 80;
+    }
+    else {
+        std::istringstream iStrStrm(argv[1]);
+        iStrStrm >> portId;
+    }
+
+    if (portId == 0 || portId > UINT16_MAX) {
+        std::cerr << "Cannot bing server to port \"" << portId << "\"";
+        return -1;
+    }
+
     try {
-        Pistache::Address addr(Pistache::Ipv4::any(), Pistache::Port(80));
+        Pistache::Address addr(Pistache::Ipv4::any(), Pistache::Port(portId));
         
         auto opts = Http::Endpoint::options().threads(1);
         Http::Endpoint server(addr);
@@ -13,6 +30,10 @@ int main() {
         std::cerr << "Server is running" << std::endl;
         server.serve();
         return 0;
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Server crashed with C++ error: " << e.what() << std::endl;
+        return -1;
     }
     catch (...) {
         std::cerr << "Server crashed with error" << std::endl;
