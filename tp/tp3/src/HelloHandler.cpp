0c7c8b410f443c3059f2dfb242b9cfa5eb4e41fe
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-09-13 13:59:07 -0400

    Made server respond only to resources /test1, /test2 and /test3.

--- tp/tp3/src/HelloHandler.cpp
+++ tp/tp3/src/HelloHandler.cpp
@@ -3,10 +3,27 @@
 #include "pistache/endpoint.h"
 
 void HelloHandler::onRequest(const Pistache::Http::Request& request, Pistache::Http::ResponseWriter response) {
+    std::cout << "ressource \"" << request.resource() << '"' << std::endl;
+
     if (request.method() == Pistache::Http::Method::Get) {
-        response.send(Pistache::Http::Code::Ok, "Hello, World\n");
+        if (request.resource() == "/test1") {
+            response.send(Pistache::Http::Code::Ok, "test 1 est bon...\n");
+        }
+        else if (request.resource() == "/test2") {
+            response.send(Pistache::Http::Code::Ok, "test 2 est bon...\n");
+        }
+        else if (request.resource() == "/test3") {
+            response.send(Pistache::Http::Code::Ok, "test 3 est bon...\n");
+        }
+        else {
+            send404(response);
+        }
     }
     else {
-        response.send(Pistache::Http::Code::Not_Found, "404 Not found\n");
+        send404(response);
     }
+}
+
+void HelloHandler::send404(Pistache::Http::ResponseWriter& response) const {
+    response.send(Pistache::Http::Code::Not_Found, "404 Not found\n");
 }
--- tp/tp3/src/HelloHandler.hpp
+++ tp/tp3/src/HelloHandler.hpp
@@ -8,4 +8,7 @@ public:
     HTTP_PROTOTYPE(HelloHandler)
 
     void onRequest(const Http::Request& request, Http::ResponseWriter response);
+
+protected:
+    void send404(Http::ResponseWriter& response) const;
 };
