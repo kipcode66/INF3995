e8889b139cecbe8aae54b77e689c06863f39fabb
Emir Khaled Belhaddad <emir-khaled.belhaddad@polymtl.ca>
2018-09-13 15:45:51 -0400

    TP4: first skeleton for the android app

---
+++ tp/tp4/app/src/main/java/ca/polymtl/inf3995/team1/tp4/FragmentType.java
@@ -0,0 +1,24 @@
+package ca.polymtl.inf3995.team1.tp4;
+
+public class FragmentType {
+    private static final FragmentType FRAGMENT_TYPES[] = new FragmentType[3];
+
+    public final String name;
+
+    static {
+        FragmentType.FRAGMENT_TYPES[0] = new FragmentType("test1");
+        FragmentType.FRAGMENT_TYPES[1] = new FragmentType("test2");
+        FragmentType.FRAGMENT_TYPES[2] = new FragmentType("test3");
+    }
+
+    public static final FragmentType getInstance(int instanceId) {
+        if (instanceId >= FragmentType.FRAGMENT_TYPES.length) {
+            throw new RuntimeException("Invalide instance id: " + instanceId);
+        }
+        return FragmentType.FRAGMENT_TYPES[instanceId];
+    }
+
+    public FragmentType(String name) {
+        this.name = name;
+    }
+}
--- tp/tp4/app/src/main/java/ca/polymtl/inf3995/team1/tp4/MainActivity.java
+++ tp/tp4/app/src/main/java/ca/polymtl/inf3995/team1/tp4/MainActivity.java
@@ -1,13 +1,47 @@
 package ca.polymtl.inf3995.team1.tp4;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
+    public static final int NUM_PAGES = 3;
+
+    private ViewPager mPager;
+    private PagerAdapter mPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Instantiate a ViewPager and a PagerAdapter.
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+    }
+
+    /**
+     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return (Fragment) TestFragment.newInstance(FragmentType.getInstance(position));
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
     }
 }
