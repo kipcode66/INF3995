18d1f73e298d5430166a7900a308384c066ae121
Soukaina El Ghazi <soukaina.elghazi@bnc.ca>
2018-09-18 11:42:40 -0400

    clean code

--- tp/tp4/app/src/main/java/ca/polymtl/inf3995/team1/tp4/MainActivity.java
+++ tp/tp4/app/src/main/java/ca/polymtl/inf3995/team1/tp4/MainActivity.java
@@ -1,7 +1,6 @@
 package ca.polymtl.inf3995.team1.tp4;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
--- tp/tp4/app/src/main/java/ca/polymtl/inf3995/team1/tp4/RequestActivity.java
+++ tp/tp4/app/src/main/java/ca/polymtl/inf3995/team1/tp4/RequestActivity.java
@@ -1,7 +1,5 @@
 package ca.polymtl.inf3995.team1.tp4;
-
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
@@ -10,14 +8,7 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
-import android.widget.TextView;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.concurrent.ExecutionException;
 
 public class RequestActivity extends Activity {
 
