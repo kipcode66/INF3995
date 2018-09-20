74647da918fcfa467d2cd155c47370d50a65986b
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-09-20 13:06:57 -0400

    Improved build.sh so that we clone and compile Pistache if it isn't there already.

--- tp/tp3/build.sh
+++ tp/tp3/build.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env sh
+
+CURRENT_DIR=$(pwd)
+PISTACHE_BUILD_DIR=$(pwd)'/lib/pistache/build'
+
+if [ ! -d "${PISTACHE_BUILD_DIR}" ]; then
+    git submodule update --init --recursive
+    mkdir -p "${PISTACHE_BUILD_DIR}"
+    cd "${PISTACHE_BUILD_DIR}"
+    cmake -DCMAKE_BUILD_TYPE=Debug "${PISTACHE_BUILD_DIR}/.."
+    make
+    cd "${CURRENT_DIR}"
+fi
+
 [ -d build ] || mkdir build
 cd build
 cmake ../src
