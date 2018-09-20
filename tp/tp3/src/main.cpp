f950afdc1e550d314830a1ceb72a80e0b70a5b78
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-09-20 10:02:19 -0400

    Added usage message when CLI args are wrong.

--- tp/tp3/src/main.cpp
+++ tp/tp3/src/main.cpp
@@ -7,18 +7,32 @@
 #include "PrintHandler.hpp"
 #include "HelloHandler.hpp"
 
+void usage() {
+    std::cerr << std::endl << "Usage: tp3 [portNum]" << std::endl <<
+        std::endl <<
+        "    portNum: Port to listen on. Default: 80" << std::endl <<
+        std::endl <<
+        "    NOTE: On Unix systems, opening ports below 1024 requires superuser privileges." << std::endl;
+}
+
 int main(int argc, char** argv) {
     uint32_t portId;
     if (argc == 1) {
         portId = 80;
     }
-    else {
+    else if (argc == 2 && std::string(argv[1]) != "-h" && std::string(argv[1]) != "--help") {
         std::istringstream iStrStrm(argv[1]);
         iStrStrm >> portId;
     }
+    else {
+        usage();
+        return 0;
+    }
 
     if (portId == 0 || portId > UINT16_MAX) {
-        std::cerr << "Cannot bing server to port \"" << portId << "\"";
+        std::cerr << std::endl <<
+            "Cannot bind server to port \"" << argv[1] << "\"" << std::endl;
+        usage();
         return -1;
     }
 
