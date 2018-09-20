bba5c8cb422c1d3aaa4b312da67a01c2b6eff35b
Soukaina El Ghazi <soukaina.elghazi@bnc.ca>
2018-09-20 12:32:12 -0400

    code refactoring

--- tp/tp4/app/src/main/java/ca/polymtl/inf3995/team1/tp4/RequestButtonPageFragment.java
+++ tp/tp4/app/src/main/java/ca/polymtl/inf3995/team1/tp4/RequestButtonPageFragment.java
@@ -19,7 +19,6 @@ import android.widget.Button;
  * create an instance of this fragment.
  */
 public class RequestButtonPageFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_REQUEST_ID = "requestId";
 
