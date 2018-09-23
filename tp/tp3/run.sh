1edbe3ef1e49f47446aea7ce8f8e394145c34df6
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-09-23 09:08:34 -0400

    Made script that tries to start the server on port unit a valid one is found

--- tp/tp3/run.sh
+++ tp/tp3/run.sh
@@ -1,4 +1,21 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 APP_NAME='tp3'
-sudo ./build/$APP_NAME
+
+# Check if root
+if [ "$(whoami)" == root ]; then
+    PORT=80
+    MAX_PORT=1023
+else
+    PORT=8080
+    MAX_PORT=8500
+fi
+
+while [ "$PORT" != "$MAX_PORT" ]; do
+    echo $'\n'"Trying port number $PORT"
+    sudo ./build/$APP_NAME "$PORT"
+    RETURN_CODE=$?
+    if [ "$RETURN_CODE" == 255 ]; then
+        PORT=$(($PORT + 1))
+    fi
+done
 
