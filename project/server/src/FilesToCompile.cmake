2dcd61a648318584474582eeb0105417481ac90b
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-09-28 10:56:44 -0400

    Configured CMake so we can have more than just a main.cpp file.

---
+++ project/server/src/FilesToCompile.cmake
@@ -0,0 +1,24 @@
+# This CMake script contains the files that will be compiled.
+# We do not use file(GLOB_RECURSE ...) because we would need to rerun CMake
+# when we pull from Git and somebody added a file.
+
+###############################################################################
+#                            C++ TRANSLATION UNITS                            #
+###############################################################################
+
+# All .cpp, .cc, .c files EXCEPT main.cpp
+# Use quotes "" if a file contains a space (although not recommended)
+set(SERVER_CPP_SOURCES
+    http/RequestHandler.cpp
+    
+)
+
+###############################################################################
+#                                    HEADER FILES                             #
+###############################################################################
+
+# All .h, .hpp, .hh files
+# Use quotes "" if a file contains a space (although not recommended)
+set(SERVER_HEADER_SOURCES
+    http/RequestHandler.hpp
+)
