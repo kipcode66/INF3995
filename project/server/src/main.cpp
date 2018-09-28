e94f3633da9838baab51c66a16041ef245665a61
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-09-28 10:57:15 -0400

    Made server's main.cpp file use the RequestHandler class.

--- project/server/src/main.cpp
+++ project/server/src/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <inttypes.h>
+
+#include "http/RequestHandler.hpp"
 
 int main() {
-    std::cout << "Server says: Hello, World!" << std::endl;
+    uint16_t portId = 80;
+    Pistache::Address addr(Pistache::Ipv4::any(), Pistache::Port(portId));
+
+    auto opts = Pistache::Http::Endpoint::options().threads(1);
+    Pistache::Http::Endpoint server(addr);
+    server.init(opts);
+
+    auto requestHandler = std::make_shared<elevation::http::RequestHandler>();
+    server.setHandler(requestHandler);
+    server.serve();
     return 0;
 }
