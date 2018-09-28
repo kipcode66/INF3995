d5a6c133c1f6f63ea9ac1aec8d24c77df2241fa1
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-09-28 11:44:32 -0400

    Made main take a port as first argument.

--- project/server/src/main.cpp
+++ project/server/src/main.cpp
@@ -3,8 +3,39 @@
 
 #include "http/RequestHandler.hpp"
 
-int main() {
-    uint16_t portId = 80;
+void usage() {
+    std::cerr << std::endl << "Usage: tp3 [portNum]" << std::endl <<
+        std::endl <<
+        "    portNum: Port to listen on. Default: 80" << std::endl <<
+        std::endl <<
+        "    NOTE: On Unix systems, opening ports below 1024 requires superuser privileges." << std::endl;
+}
+
+uint16_t parseArgs(int argc, char** argv) {
+    uint16_t portId;
+    if (argc == 1) {
+        portId = 80;
+    }
+    else if (argc == 2 && std::string(argv[1]) != "-h" && std::string(argv[1]) != "--help") {
+        std::istringstream iStrStrm(argv[1]);
+        iStrStrm >> portId;
+    }
+    else {
+        usage();
+        exit(0);
+    }
+
+    if (portId == 0 || portId > UINT16_MAX) {
+        std::cerr << std::endl <<
+            "Cannot bind server to port \"" << argv[1] << "\"" << std::endl;
+        usage();
+        exit(254);
+    }
+}
+
+int main(int argc, char** argv) {
+
+    uint16_t portId = parseArgs(argc, argv);
 
     try {
         Pistache::Address addr(Pistache::Ipv4::any(), Pistache::Port(portId));
@@ -15,6 +46,8 @@ int main() {
 
         auto requestHandler = std::make_shared<elevation::http::RequestHandler>();
         server.setHandler(requestHandler);
+
+        std::cout << "Server is about to start." << std::endl;
         server.serve();
     }
     catch (std::exception& e) {
@@ -23,7 +56,7 @@ int main() {
     }
     catch (...) {
         std::cerr << "Server crashed with unknown exception type" << std::endl;
-        return 254;
+        return 255;
     }
     return 0;
 }
