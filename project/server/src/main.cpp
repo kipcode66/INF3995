27fcd873d9480a9b8be8c4b53bb6a1ba19bc834f
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-09-28 11:06:11 -0400

    Made the server catch exceptions and crash with a meaningful error.

--- project/server/src/main.cpp
+++ project/server/src/main.cpp
@@ -5,14 +5,25 @@
 
 int main() {
     uint16_t portId = 80;
-    Pistache::Address addr(Pistache::Ipv4::any(), Pistache::Port(portId));
 
-    auto opts = Pistache::Http::Endpoint::options().threads(1);
-    Pistache::Http::Endpoint server(addr);
-    server.init(opts);
+    try {
+        Pistache::Address addr(Pistache::Ipv4::any(), Pistache::Port(portId));
 
-    auto requestHandler = std::make_shared<elevation::http::RequestHandler>();
-    server.setHandler(requestHandler);
-    server.serve();
+        auto opts = Pistache::Http::Endpoint::options().threads(1);
+        Pistache::Http::Endpoint server(addr);
+        server.init(opts);
+
+        auto requestHandler = std::make_shared<elevation::http::RequestHandler>();
+        server.setHandler(requestHandler);
+        server.serve();
+    }
+    catch (std::exception& e) {
+        std::cerr << "Server crashed with C++ exception: \"" << e.what() << "\"" << std::endl;
+        return 255;
+    }
+    catch (...) {
+        std::cerr << "Server crashed with unknown exception type" << std::endl;
+        return 254;
+    }
     return 0;
 }
