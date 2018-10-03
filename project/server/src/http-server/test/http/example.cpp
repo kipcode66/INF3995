4decb3329d8f2c372618525426c3d565c43fa0e6
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-10-02 20:48:27 -0400

    Added use inside the unit tests of a symbol that we defined.

--- project/server/src/http-server/test/http/example.cpp
+++ project/server/src/http-server/test/http/example.cpp
@@ -3,10 +3,19 @@
 #define BOOST_TEST_MODULE EXAMPLE
 #include <boost/test/unit_test.hpp>
 
+#include "http/RequestHandler.hpp"
+
+namespace elevation {
+namespace http {
+
 BOOST_AUTO_TEST_CASE(passing_test_case) {
+    RequestHandler requestHandler = RequestHandler();
     BOOST_TEST(1 == 1);
 }
 
 BOOST_AUTO_TEST_CASE(failing_test_case) {
     BOOST_TEST(1 == 2);
 }
+
+} // namespace http
+} // namespace elevation
