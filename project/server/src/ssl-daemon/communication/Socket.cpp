a5941f248843de8cdb29acee6b91980c41cf2a1d
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-10-04 13:15:00 -0400

    Addedtwo separate methods for Socket creation.

--- project/server/src/ssl-daemon/communication/Socket.cpp
+++ project/server/src/ssl-daemon/communication/Socket.cpp
@@ -1,13 +1,31 @@
 #include "Socket.hpp"
 
+#include <unistd.h>
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <cstring>
 
 namespace elevation {
 namespace daemon {
     
-Socket::Socket(uint16_t portNum)
+Socket::Socket(uint16_t portNum, bool isLocalhostSocket)
 {
     m_fd = NO_FD;
+
+    if (isLocalhostSocket) {
+        createLocalSocket_(portNum);
+    }
+    else {
+        createIpSocket_(portNum);
+    }
+}
+
+Socket::~Socket()
+{
+    if (m_fd != NO_FD) {
+        ::close(m_fd);
+    }
 }
 
 Socket& Socket::operator<<(const std::string& str) {
@@ -16,6 +34,12 @@ Socket& Socket::operator<<(const std::string& str) {
 
 Socket& Socket::operator>>(std::string& str) {
     return *this;
+}
+
+void Socket::createLocalSocket_(uint16_t portNum) {
+}
+
+void Socket::createIpSocket_(uint16_t portNum) {
 }
 
 } // namespace daemon
--- project/server/src/ssl-daemon/communication/Socket.hpp
+++ project/server/src/ssl-daemon/communication/Socket.hpp
@@ -12,9 +12,11 @@ public:
     static constexpr int NO_FD = -1;
 
 public:
-    Socket(uint16_t portNum);
+    Socket(uint16_t portNum, bool isLocalhostSocket);
     Socket(const Socket& other) = delete;
     Socket(Socket&& other) = delete;
+
+    virtual ~Socket();
 
     Socket& operator=(const Socket& other) = delete;
     Socket& operator=(Socket&& other) = delete;
@@ -22,7 +24,11 @@ public:
     Socket& operator<<(const std::string& str);
     Socket& operator>>(std::string& str);
 
-private:
+protected:
+    void createLocalSocket_(uint16_t portNum);
+    void createIpSocket_(uint16_t portNum);
+
+protected:
     int m_fd;
 };
 
