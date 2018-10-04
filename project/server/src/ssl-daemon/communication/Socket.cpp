e28c760d41bf3d328b852c43f22c1f98207993e3
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-10-04 16:31:07 -0400

    Began implementing the stream and extraction operators on Socket.

--- project/server/src/ssl-daemon/communication/Socket.cpp
+++ project/server/src/ssl-daemon/communication/Socket.cpp
@@ -1,6 +1,10 @@
 #include "Socket.hpp"
 
 #include <unistd.h>
+#include <sstream>
+#include <cstring>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 
 namespace elevation {
 namespace daemon {
@@ -17,10 +21,29 @@ Socket::~Socket()
 }
 
 Socket& Socket::operator<<(const std::string& str) {
+    ::write(m_fd, str.c_str(), str.size() + 1);
     return *this;
 }
 
 Socket& Socket::operator>>(std::string& str) {
+    constexpr int READ_SIZE = 1024;
+    char buffer[READ_SIZE + 1];
+
+    std::ostringstream oStringStream;
+    int bytesRead = 0;
+    char* currentBuffer = buffer;
+    while (bytesRead < READ_SIZE && bytesRead >= 0) {
+        bytesRead += ::read(m_fd, buffer, READ_SIZE - bytesRead);
+    }
+
+    if (bytesRead >= 0) {
+        buffer[bytesRead] = '\0';
+        oStringStream << buffer;
+        str = oStringStream.str();
+    }
+    else {
+        throw std::runtime_error(::strerror(errno));
+    }
     return *this;
 }
 
