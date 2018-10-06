28b5b11f3f6fdb3be972106c20e8cce9d0e9d1d0
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-10-05 21:17:29 -0400

    First implementation of the DaemonRunner.

--- project/server/src/ssl-daemon/core/DaemonRunner.cpp
+++ project/server/src/ssl-daemon/core/DaemonRunner.cpp
@@ -1,4 +1,7 @@
 #include "DaemonRunner.hpp"
+#include "signal/SignalHandling.hpp"
+#include "communication/ListenerSocket.hpp"
+#include "ssl/SslContext.hpp"
 
 namespace elevation {
 namespace daemon {
@@ -8,7 +11,18 @@ DaemonRunner::DaemonRunner(const ArgsParser::Config& config)
 { }
 
 void DaemonRunner::run() {
-    
+    ListenerSocket listenerSocket(m_config.getListenPort());
+
+    while (true) {
+        SslContext& ctx = SslContext::getInstance();
+        IpSocket nextClient = listenerSocket.accept();
+        SslSession session = ctx.makeSession();
+        session.bindTo(nextClient);
+        
+        if (SignalHandling::cleanupRequested.load()) {
+            throw std::runtime_error("SSL Daemon killed by signal.");
+        }
+    }
 }
 
 }
