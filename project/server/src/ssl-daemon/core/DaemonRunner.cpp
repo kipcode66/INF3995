4b07e5a3b4eaed790c61e4849d3fd86616cce40b
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-10-08 19:12:17 -0400

    Implemented writing to client from the HTTP server.

--- project/server/src/ssl-daemon/core/DaemonRunner.cpp
+++ project/server/src/ssl-daemon/core/DaemonRunner.cpp
@@ -28,28 +28,64 @@ void DaemonRunner::runner_(SslSession clientSession, ClientSocket httpServerSock
 
 void DaemonRunner::reader_(SslSession& clientSession, ClientSocket& httpServerSocket) {
     try {
-        std::string data = clientSession.read();
-        httpServerSocket << data;
+        while (true) {
+            std::string data = clientSession.read();
+            httpServerSocket << data;
+            std::cout << "Packet read done" << std::endl;
+        }
     }
     catch (const SocketClosedException& e) {
         std::cerr << "Reader thread : Socket closed." << std::endl;
     }
     catch (const std::exception& e) {
         std::cerr << "C++ exception thrown in reader thread : " << e.what() << std::endl;
+        // TODO Close socket cleanly.
     }
     catch (...) {
         std::cerr << "Unknown exception thrown in reader thread." << std::endl;
+        // TODO Close socket cleanly.
     }
+
+    std::cout << "Reader done." << std::endl;
 }
 
 void DaemonRunner::writer_(SslSession& clientSession, ClientSocket& httpServerSocket) {
-    clientSession.write(
-        "HTTP/1.1 404 Not Found\n"
-        "Connection: Keep-Alive\n"
-        "Content-Length: 13\n"
-        "\n"
-        "404 not found"
-    );
+    try {
+        while (true) {
+            const std::regex HTTP_HEADER_END_REGEX("^\r?\n$");
+            const std::regex HTTP_CONTENT_SIZE_REGEX("^Content-Length:\\s*(\\d+)", std::regex_constants::icase);
+            const std::size_t HTTP_CONTENT_SIZE_RESULT_GROUP_ID = 1;
+
+            std::size_t httpBodySize = 0;
+            std::string line = httpServerSocket.readLine();
+            while (!std::regex_search(line, HTTP_HEADER_END_REGEX)) {
+                std::smatch matchResult;
+                if (std::regex_search(line, matchResult, HTTP_CONTENT_SIZE_REGEX)) {
+                    httpBodySize = std::stoi(matchResult[HTTP_CONTENT_SIZE_RESULT_GROUP_ID]);
+                }
+
+                clientSession.write(line);
+                line = httpServerSocket.readLine();
+            }
+            clientSession.write(line);
+            std::string data = httpServerSocket.read(httpBodySize);
+            clientSession.write(data);
+            std::cout << "Packet write done" << std::endl;
+        }
+    }
+    catch (const SocketClosedException& e) {
+        std::cout << "Writer thread : Socket closed." << std::endl;
+    }
+    catch (const std::exception& e) {
+        std::cerr << "C++ exception thrown in writer thread : " << e.what() << std::endl;
+        // TODO Close socket cleanly.
+    }
+    catch (...) {
+        std::cerr << "Unknown exception thrown in writer thread." << std::endl;
+        // TODO Close socket cleanly.
+    }
+
+    std::cout << "Writer done." << std::endl;
 }
 
 } // namespace daemon
