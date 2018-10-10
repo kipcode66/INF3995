36e6ae84849d628070e159e6f4cfcd106bd3a360
Soukaina El Ghazi <soukaina.elghazi@bnc.ca>
2018-10-09 21:07:53 -0400

    implementation of the activity to get the local songs

--- project/client/app/src/main/java/ca/polymtl/inf3990_01/client/view/LocalSongActivity.kt
+++ project/client/app/src/main/java/ca/polymtl/inf3990_01/client/view/LocalSongActivity.kt
@@ -1,85 +1,85 @@
 package ca.polymtl.inf3990_01.client.view
-
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.design.widget.NavigationView
-import android.support.v4.view.GravityCompat
-import android.support.v7.app.ActionBarDrawerToggle
+import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import ca.polymtl.inf3990_01.client.R
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
 import kotlinx.android.synthetic.main.activity_local_song.*
-import kotlinx.android.synthetic.main.app_bar_local_song.*
+import ca.polymtl.inf3990_01.client.R
+import kotlinx.android.synthetic.main.song.view.*
 
-class LocalSongActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+class LocalSongActivity : AppCompatActivity(){
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_local_song)
-        setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-
-        val toggle = ActionBarDrawerToggle(
-                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawer_layout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        nav_view.setNavigationItemSelectedListener(this)
+        verifyPermsions()
     }
 
-    override fun onBackPressed() {
-        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
-            drawer_layout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
+    var songsList=ArrayList<Song>()
+    var adapter:SongAdapter?=null
+
+    inner  class SongAdapter: BaseAdapter {
+
+        var listOfSongOnTheDevice = ArrayList<Song>()
+        constructor(listOfSongOnTheDevice: ArrayList<Song>) : super() {
+            this.listOfSongOnTheDevice = listOfSongOnTheDevice
         }
-    }
+        override fun getView(postion: Int, view: View?, viewGroup: ViewGroup?): View {
+            val view = layoutInflater.inflate(R.layout.song, null)
+            val song = this.listOfSongOnTheDevice[postion]
+            view.songName.text = song.title
+            view.author.text = song.authorName
+            view.send.setOnClickListener(View.OnClickListener {
+                //TODO : send song to server
+            })
+            return view
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.local_song, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            R.id.action_settings -> return true
-            else -> return super.onOptionsItemSelected(item)
         }
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        // Handle navigation view item clicks here.
-        when (item.itemId) {
-            R.id.nav_camera -> {
-                // Handle the camera action
-            }
-            R.id.nav_gallery -> {
-
-            }
-            R.id.nav_slideshow -> {
-
-            }
-            R.id.nav_manage -> {
-
-            }
-            R.id.nav_share -> {
-
-            }
-            R.id.nav_send -> {
-
-            }
+        override fun getItemId(p: Int): Long {
+            return  p.toLong()
         }
 
-        drawer_layout.closeDrawer(GravityCompat.START)
-        return true
+        override fun getCount(): Int {
+            return this.listOfSongOnTheDevice.size
+        }
+        override fun getItem(item: Int): Any {
+            return this.listOfSongOnTheDevice[item]
+        }
+    }
+    private val CODE_PERMISSIONS = 133
+
+    fun verifyPermsions() {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                        CODE_PERMISSIONS)
+                return
+            }
+        foundSong()
+
+    }
+    fun foundSong() {
+        val songsURI = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+        val music = MediaStore.Audio.Media.IS_MUSIC + "!=0"
+        val cursor = contentResolver.query(songsURI, null, music, null, null)
+        if (cursor != null) {
+            if (cursor!!.moveToFirst()) {
+                do {
+                    val authorName = cursor!!.getString(cursor!!.getColumnIndex(MediaStore.Audio.Media.ARTIST))
+                    val songTitle = cursor!!.getString(cursor!!.getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME))
+                    songsList.add(Song(songTitle, authorName))
+                } while (cursor!!.moveToNext())
+            }
+            cursor!!.close()
+            adapter = SongAdapter(songsList)
+            SongsList.adapter = adapter
+        }
     }
 }
+
---
+++ project/client/app/src/main/java/ca/polymtl/inf3990_01/client/view/Song.kt
@@ -0,0 +1,10 @@
+package ca.polymtl.inf3990_01.client.view
+
+class Song{
+    var title:String?=null
+    var authorName:String?=null
+    constructor(title:String,authorName:String){
+        this.title=title
+        this.authorName=authorName
+    }
+}
