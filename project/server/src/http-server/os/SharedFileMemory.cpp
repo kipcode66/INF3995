44307ee3b018fbcea948a044563834a94688ee99
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-10-22 22:30:04 -0400

    Created method getFileSize_ in SharedFileMemory.

--- project/server/src/http-server/os/SharedFileMemory.cpp
+++ project/server/src/http-server/os/SharedFileMemory.cpp
@@ -1,5 +1,7 @@
 #include "SharedFileMemory.hpp"
 
+#include <fstream>
+#include <stdexcept>
 #include <sys/mman.h>
 #include <sys/stat.h>
 
@@ -13,6 +15,15 @@ SharedFileMemory::~SharedFileMemory() { }
 
 SharedFileMemory& SharedFileMemory::operator=(SharedFileMemory&& that) {
     return *this;
+}
+
+std::size_t SharedFileMemory::getFileSize_(const std::string& fileName) const {
+    std::ifstream file(fileName, std::ifstream::binary | std::ifstream::ate);
+    if (!file.is_open()) {
+        throw std::runtime_error(std::string("Could not open ") + fileName);
+    }
+    std::size_t fileSize = file.tellg();
+    return fileSize;
 }
 
 } // namespace elevation
