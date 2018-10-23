e0b034d0b40c4d368c074a6978671fded432ecd4
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-10-23 16:03:07 -0400

    Done RAII part of PulseDevice.

--- project/server/src/http-server/mp3/player/pulse/PulseDevice.cpp
+++ project/server/src/http-server/mp3/player/pulse/PulseDevice.cpp
@@ -4,12 +4,30 @@ namespace elevation {
 
 PulseDevice::PulseDevice()
 {
+    // TODO
+}
 
+PulseDevice::PulseDevice(PulseDevice&& that)
+{
+    operator=(std::move(that));
 }
 
 PulseDevice::~PulseDevice()
 {
+    cleanup_();
+}
 
+PulseDevice& PulseDevice::operator=(PulseDevice&& that) {
+    cleanup_();
+    this->m_device = that.m_device;
+    that .m_device = nullptr;
+    return *this;
+}
+
+void PulseDevice::cleanup_() {
+    if (m_device != nullptr) {
+        // TODO Cleanup
+    }
 }
 
 } // namespace elevation
