fbbf94a2ecec1223c1f1328f9f58f4eb6d241b61
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-10-24 18:17:16 -0400

    Created class MadAudioFormatter.

---
+++ project/server/src/http-server/mp3/player/mad/MadAudioFormatter.hpp
@@ -0,0 +1,14 @@
+#ifndef MP3_PLAYER_FORMAT_AUDIOFORMATTER_HPP
+#define MP3_PLAYER_FORMAT_AUDIOFORMATTER_HPP
+
+namespace elevation {
+
+class MadAudioFormatter {
+public:
+    MadAudioFormatter();
+    ~MadAudioFormatter();
+};
+
+} // namespace elevation
+
+#endif // !MP3_PLAYER_FORMAT_AUDIOFORMATTER_HPP
