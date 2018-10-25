80ca9aa3b32674bea5030acea1c80c0a329eebd8
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-10-24 20:57:40 -0400

    Made the Mp3Player start a thread when requested to play a file.

--- project/server/src/http-server/mp3/player/Mp3Player.hpp
+++ project/server/src/http-server/mp3/player/Mp3Player.hpp
@@ -2,18 +2,22 @@
 #define MP3_PLAYER_MP3PLAYER_HPP
 
 #include <string>
+#include <thread>
 
 namespace elevation {
 
 class Mp3Player {
 protected:
-    static void run_(const std::string& fileName);
+    static void run_(std::string fileName);
 
 public:
     explicit Mp3Player();
     virtual ~Mp3Player();
 
     void startPlaying(const std::string& fileName);
+
+private:
+    std::thread m_playing;
 };
 
 } // namespace elevation
