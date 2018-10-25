532542d7719c88b29fe87c240c62e804a899293c
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-10-24 20:57:40 -0400

    Made the Mp3Player start a thread when requested to play a file.

--- project/server/src/http-server/mp3/player/Mp3Player.cpp
+++ project/server/src/http-server/mp3/player/Mp3Player.cpp
@@ -1,11 +1,11 @@
 #include "Mp3Player.hpp"
 
-#include <thread>
+#include <iostream>
 
 namespace elevation {
 
-void Mp3Player::run_(const std::string& fileName) {
-
+void Mp3Player::run_(std::string fileName) {
+    std::cout << __PRETTY_FUNCTION__ << ':' << fileName << std::endl;
 }
 
 Mp3Player::Mp3Player() { }
@@ -13,7 +13,10 @@ Mp3Player::Mp3Player() { }
 Mp3Player::~Mp3Player() { }
 
 void Mp3Player::startPlaying(const std::string& fileName) {
-
+    if (!m_playing.joinable()) {
+        throw std::runtime_error("Elevation MP3 player cannot start playing MP3 : Already playing");
+    }
+    m_playing = std::thread(&Mp3Player::run_, fileName);
 }
 
 } // namespace elevation
