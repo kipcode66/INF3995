948ed4be20844872ca3791d3fdf98296f5749e97
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-10-25 10:18:12 -0400

    Added move sematics to Mp3Player.

--- project/server/src/http-server/mp3/player/Mp3Player.hpp
+++ project/server/src/http-server/mp3/player/Mp3Player.hpp
@@ -10,7 +10,13 @@ namespace elevation {
 class Mp3Player {
 public:
     explicit Mp3Player();
+    Mp3Player(const Mp3Player&) = delete;
+    Mp3Player(Mp3Player&& that);
+
     virtual ~Mp3Player();
+
+    Mp3Player& operator=(const Mp3Player&) = delete;
+    Mp3Player& operator=(Mp3Player&& that);
 
     void startPlaying(const std::string& fileName);
     void waitUntilSongFinished();
