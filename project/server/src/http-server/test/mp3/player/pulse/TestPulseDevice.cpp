6e6721bfd0ba79e03c7f381ece7408c3b170f149
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-10-25 17:13:42 -0400

    Made the play test timeout after 7 seconds.

--- project/server/src/http-server/test/mp3/player/pulse/TestPulseDevice.cpp
+++ project/server/src/http-server/test/mp3/player/pulse/TestPulseDevice.cpp
@@ -29,22 +29,32 @@ BOOST_FIXTURE_TEST_CASE(moveAssignment, TestFixture) {
     movedPulse = std::move(m_pulse);
 }
 
-BOOST_FIXTURE_TEST_CASE(playSanityCheck, TestFixture) {
-    SharedFileMemory fileMemory(TIPPERARY_SHORT_WAV_PATH);
-    constexpr std::size_t MAX_FRAME_LENGTH = 2 << 10;
-    std::vector<uint8_t> buffer;
-    std::size_t currentPosition = 0;
-    while (currentPosition < fileMemory.size() - MAX_FRAME_LENGTH) {
-        std::size_t frameLength = std::min(fileMemory.size() - currentPosition, MAX_FRAME_LENGTH);
-        buffer.resize(0);
-        std::copy(
-            &fileMemory[currentPosition],
-            &fileMemory[currentPosition + frameLength - 1] + 1,
-            std::back_inserter(buffer)
-        );
-        m_pulse.play(buffer);
-        currentPosition += frameLength;
-    }
+constexpr unsigned int TIPPERARY_SHORT_TIMEOUT_SECONDS = 7;
+BOOST_AUTO_TEST_CASE(playSanityCheck, *boost::unit_test::timeout(TIPPERARY_SHORT_TIMEOUT_SECONDS)) {
+    // Creating a class manually because it seems only BOOST_AUTO_TEST_CASE has a timeout option.
+    class TestCase : public TestFixture {
+    public:
+        void runTest() {
+            SharedFileMemory fileMemory(TIPPERARY_SHORT_WAV_PATH);
+            constexpr std::size_t MAX_FRAME_LENGTH = 2 << 10;
+            std::vector<uint8_t> buffer;
+            std::size_t currentPosition = 0;
+            while (currentPosition < fileMemory.size() - MAX_FRAME_LENGTH) {
+                std::size_t frameLength = std::min(fileMemory.size() - currentPosition, MAX_FRAME_LENGTH);
+                buffer.resize(0);
+                std::copy(
+                    &fileMemory[currentPosition],
+                    &fileMemory[currentPosition + frameLength - 1] + 1,
+                    std::back_inserter(buffer)
+                );
+                m_pulse.play(buffer);
+                currentPosition += frameLength;
+            }
+        }
+    };
+
+    TestCase tc;
+    tc.runTest();
 }
 
 } // namespace elevation
