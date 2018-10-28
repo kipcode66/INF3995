0c5450d235411694b481208573f4c40dcd9e273c
Emir Khaled Belhaddad <emir-khaled.belhaddad@polymtl.ca>
2018-10-28 18:36:03 -0400

    f344: Added class FileCache's skeleton.

---
+++ project/server/src/http-server/filesystem/FileCache.cpp
@@ -0,0 +1,42 @@
+#include "FileCache.hpp"
+
+namespace elevation {
+
+FileCache::FileCache(const std::string& cachePath)
+    :m_path(cachePath)
+{
+    ensureCacheDirCreated_();
+}
+
+FileCache::FileCache(const std::experimental::filesystem::path::path& cachePath)
+    :m_path(cachePath)
+{
+    ensureCacheDirCreated_();
+}
+
+FileCache::~FileCache() {
+    wipeCachedFiles_();
+}
+
+size_t FileCache::fileCount() const {
+}
+
+size_t FileCache::cacheSize() const {
+}
+
+std::vector<std::experimental::filesystem::path::path> FileCache::getFileList() const {
+}
+
+void FileCache::setFileContent(const std::string& fileName, const std::string& data) {
+}
+
+void FileCache::deleteFile(const std::string& fileName) {
+}
+
+bool FileCache::isFileCached(const std::string& fileName) {
+}
+
+std::experimental::filesystem::directory_entry FileCache::getFile(const std::string& fileName) const {
+}
+
+} // namespace elevation
---
+++ project/server/src/http-server/filesystem/FileCache.hpp
@@ -0,0 +1,36 @@
+#ifndef SRC_HTTP_FS_FILECACHE_HPP
+#define SRC_HTTP_FS_FILECACHE_HPP
+
+#include <string>
+#include <experimental/filesystem>
+
+namespace elevation {
+
+class FileCache {
+public:
+    explicit FileCache(const std::string& cachePath);
+    FileCache(const std::experimental::filesystem::path::path& cachePath);
+    FileCache(const FileCache&) = delete;
+    virtual ~FileCache();
+
+    FileCache& operator=(const FileCache&) = delete;
+
+    size_t fileCount() const;
+    size_t cacheSize() const;
+    std::vector<std::experimental::filesystem::path::path> getFileList() const;
+    void setFileContent(const std::string& fileName, const std::string& data);
+    void deleteFile(const std::string& fileName);
+    bool isFileCached(const std::string& fileName);
+    std::experimental::filesystem::directory_entry getFile(const std::string& fileName) const;
+
+protected:
+    void ensureCacheDirCreated_();
+    void wipeCachedFiles_();
+
+protected:
+    const std::experimental::filesystem::path::path& m_path;
+};
+
+} // namespace elevation
+
+#endif // !SRC_HTTP_FS_FILECACHE_HPP
