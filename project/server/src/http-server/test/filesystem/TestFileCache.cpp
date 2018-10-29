61e47ba1248786452c63c9b3846445e03eb4d83b
Emir Khaled Belhaddad <emir-khaled.belhaddad@polymtl.ca>
2018-10-28 22:30:05 -0400

    f344: Fixed compilation errors.

--- project/server/src/http-server/test/filesystem/TestFileCache.cpp
+++ project/server/src/http-server/test/filesystem/TestFileCache.cpp
@@ -13,13 +13,13 @@ namespace fs = std::experimental::filesystem;
  * Cleans up after all FileCache test case.
  */
 void cleanup() {
-    fs::path fileCacheDir(FILE_CACHE_TEST_DIRECTORY);
+    fs::path fileCacheDir = std::string(FILE_CACHE_TEST_DIRECTORY);
     // Ensure the cache directory is deleted.
     fs::remove_all(fileCacheDir);
 }
 
 BOOST_AUTO_TEST_CASE(simpleConstructionDestruction) {
-    fs::path fileCacheDir(std::string(FILE_CACHE_TEST_DIRECTORY));
+    fs::path fileCacheDir = std::string(FILE_CACHE_TEST_DIRECTORY);
     // Ensure the cache directory is deleted.
     fs::remove_all(fileCacheDir);
 
@@ -31,19 +31,19 @@ BOOST_AUTO_TEST_CASE(simpleConstructionDestruction) {
 }
 
 BOOST_AUTO_TEST_CASE(afterCrashConstructionDestruction) {
-    fs::path fileCacheDir(FILE_CACHE_TEST_DIRECTORY);
-    fs::path dummyMp3SourceFile(TEST_MP3_PATH);
-    fs::path dummyMp3DestinationFile(FileCache);
+    fs::path fileCacheDir = std::string(FILE_CACHE_TEST_DIRECTORY);
+    fs::path dummyMp3SourceFile = TEST_MP3_PATH;
+    fs::path dummyMp3DestinationFile = fileCacheDir;
     dummyMp3DestinationFile /= dummyMp3SourceFile.filename();
     // Ensure the cache directory exists.
     fs::create_directories(fileCacheDir);
     // Copy a file over
     fs::copy_file(dummyMp3SourceFile, dummyMp3DestinationFile);
-    BOOST_REQUIRE(fs::exists(dummyMp3DestinationFile))
+    BOOST_REQUIRE(fs::exists(dummyMp3DestinationFile));
 
     FileCache* fileCache = new FileCache(std::string(FILE_CACHE_TEST_DIRECTORY));
     BOOST_TEST(fs::exists(fileCacheDir));
-    BOOST_TEST(!fs::exists(dummyMp3DestinationFile))
+    BOOST_TEST(!fs::exists(dummyMp3DestinationFile));
     delete fileCache;
     BOOST_TEST(!fs::exists(fileCacheDir));
     cleanup();
