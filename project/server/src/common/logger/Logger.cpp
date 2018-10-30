ad2bbbab4c0aa0e2c9cc845b09d2d8643eb2e6a1
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-10-30 14:05:04 -0400

    Made the Logger able to create a file on construction.

--- project/server/src/common/logger/Logger.cpp
+++ project/server/src/common/logger/Logger.cpp
@@ -10,16 +10,22 @@ namespace elevation {
 
 Logger::Logger(const std::string& prefix)
     : m_prefix(prefix)
+    , m_logFile(getFileName_(prefix), std::ios_base::trunc)
 { }
 
 Logger::~Logger() { }
 
+std::string Logger::getFileName_(const std::string& prefix) {
+    std::ostringstream osStream;
+    osStream << SERVER_LOG_DIR << '/' << prefix << '-' << getTime_() << '.' << FILENAME_EXTENSION;
+    return osStream.str();
 }
 
-Logger::getFileName_() const {
-
+std::string Logger::getTime_() {
+    std::time_t now = std::time(nullptr);
+    std::ostringstream osStream;
+    osStream << std::put_time<char>(std::localtime(&now), TIME_FORMAT);
+    return osStream.str();
 }
 
-Logger::getTime_() const {
-
-}
+} // namespace elevation
