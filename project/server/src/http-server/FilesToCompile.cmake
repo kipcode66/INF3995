784a38e7e07cab3c5d49b4f9e8fd7e87996cdfca
Emir Khaled Belhaddad <emir-khaled.belhaddad@polymtl.ca>
2018-10-30 18:42:43 -0400

    f344: Changed path to compile for moved files.

--- project/server/src/http-server/FilesToCompile.cmake
+++ project/server/src/http-server/FilesToCompile.cmake
@@ -12,8 +12,8 @@ include("${CMAKE_SCRIPTS_DIR}/FilesToCompileFunctions.cmake")
 # Use quotes "" if a file contains a space (although not recommended)
 set(HTTP_SERVER_CPP_SOURCES
     http/RestApi.cpp
-    mp3-header/Mp3Duration.cpp
-    mp3-header/Mp3Header.cpp
+    mp3/header/Mp3Duration.cpp
+    mp3/header/Mp3Header.cpp
     mp3/player/Mp3Player.cpp
     mp3/player/mad/MadDecoder.cpp
     mp3/player/mad/MadAudioFormatter.cpp
@@ -31,8 +31,8 @@ set(HTTP_SERVER_CPP_SOURCES
 # Use quotes "" if a file contains a space (although not recommended)
 set(HTTP_SERVER_HEADER_SOURCES
     http/RestApi.hpp
-    mp3-header/Mp3Duration.hpp
-    mp3-header/Mp3Header.hpp
+    mp3/header/Mp3Duration.hpp
+    mp3/header/Mp3Header.hpp
     mp3/player/Mp3Player.hpp
     mp3/player/mad/MadDecoder.hpp
     mp3/player/pulse/PulseDevice.hpp
