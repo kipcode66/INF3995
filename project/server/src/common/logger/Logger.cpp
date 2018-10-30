d6b5979be3719e442365056ab8e6642981408f9c
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-10-30 13:56:16 -0400

    Began adding attributes and methods to Logger.

--- project/server/src/common/logger/Logger.cpp
+++ project/server/src/common/logger/Logger.cpp
@@ -1,13 +1,25 @@
 #include "Logger.hpp"
 
+#include <elevation/common/Config.hpp>
+
+#include <chrono>
+#include <iomanip>
+#include <sstream>
+
 namespace elevation {
 
-Logger::Logger(const std::string& prefix) {
+Logger::Logger(const std::string& prefix)
+    : m_prefix(prefix)
+{ }
+
+Logger::~Logger() { }
 
 }
 
-Logger::~Logger() {
+Logger::getFileName_() const {
 
 }
+
+Logger::getTime_() const {
 
 }
