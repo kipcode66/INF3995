01829ae6106b70d809dbd7fcf1be3ce38239be05
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-11-01 13:27:21 -0400

    Fixed the PendingSongs class so that the tests pass.

--- project/server/src/http-server/mp3/list/PendingSongs.cpp
+++ project/server/src/http-server/mp3/list/PendingSongs.cpp
@@ -1,5 +1,7 @@
 #include "PendingSongs.hpp"
 
+#include <algorithm>
+
 namespace elevation {
 
 PendingSongs::PendingSongs(std::size_t maxSongs) {
@@ -20,16 +22,22 @@ void PendingSongs::addSong(const std::experimental::filesystem::path& songPath) 
 
 void PendingSongs::removeSong(const std::experimental::filesystem::path& songPath) {
     std::lock_guard<std::mutex> lock(m_mutex);
-    m_pendingSongs.remove(songPath);
+    auto it = std::find(m_pendingSongs.begin(), m_pendingSongs.end(), songPath);
+    if (it != m_pendingSongs.end()) {
+        m_pendingSongs.remove(songPath);
+    }
+    else {
+        throw std::out_of_range("Cannot remove song : No song under path " + songPath.string());
+    }
 }
 
 void PendingSongs::reorderSongs(std::size_t songAPosition, std::size_t songBPosition) {
     std::lock_guard<std::mutex> lock(m_mutex);
-    auto songA = m_pendingSongs.begin();
-    auto songB = m_pendingSongs.begin();
-    std::advance(songA, songAPosition);
-    std::advance(songB, songBPosition);
-    if (songA != m_pendingSongs.end() && songB != m_pendingSongs.end()) {
+    if (songAPosition < m_pendingSongs.size() && songBPosition < m_pendingSongs.size()) {
+        auto songA = m_pendingSongs.begin();
+        auto songB = m_pendingSongs.begin();
+        std::advance(songA, songAPosition);
+        std::advance(songB, songBPosition);
         std::iter_swap(songA, songB);
     }
     else {
