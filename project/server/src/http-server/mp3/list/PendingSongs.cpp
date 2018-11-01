656c1bff718576b5e546536cf20d2a188ce6f56b
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-11-01 11:07:18 -0400

    Implemented PendingSongs in a thread-unsafe manner.

--- project/server/src/http-server/mp3/list/PendingSongs.cpp
+++ project/server/src/http-server/mp3/list/PendingSongs.cpp
@@ -4,20 +4,31 @@ namespace elevation {
 
 const std::experimental::filesystem::path PendingSongs::SONGS_DIR = ".";
 
-PendingSongs::PendingSongs() { }
+PendingSongs::PendingSongs(std::size_t maxSongs) {
+    m_maxSongs = maxSongs;
+}
 
 PendingSongs::~PendingSongs() { }
 
 void PendingSongs::addSong(const std::string& songName) {
-
+    if (m_pendingSongs.size() < m_maxSongs) {
+        m_pendingSongs.push_back(SONGS_DIR / songName);
+    }
+    else {
+        throw std::out_of_range("Cannot add a new song : Already at full capacity.");
+    }
 }
 
 void PendingSongs::removeSong(const std::string& songName) {
-
+    m_pendingSongs.remove(SONGS_DIR / songName);
 }
 
-void PendingSongs::reorderSong(const std::string& songName, std::size_t newPosition) {
-
+void PendingSongs::reorderSong(std::size_t songAPosition, std::size_t songBPosition) {
+    auto songA = m_pendingSongs.begin();
+    auto songB = m_pendingSongs.begin();
+    std::advance(songA, songAPosition);
+    std::advance(songB, songBPosition);
+    std::swap(songA, songB);
 }
 
 } // namespace elevation
