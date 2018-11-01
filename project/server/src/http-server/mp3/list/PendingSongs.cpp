c965348d42124dd92d9489459cad15fb053bf124
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-11-01 10:33:51 -0400

    Created class PendingSongs.

---
+++ project/server/src/http-server/mp3/list/PendingSongs.cpp
@@ -0,0 +1,21 @@
+#include "PendingSongs.hpp"
+
+namespace elevation {
+
+PendingSongs::PendingSongs() { }
+
+PendingSongs::~PendingSongs() { }
+
+void PendingSongs::addSong(/* TODO */) {
+
+}
+
+void PendingSongs::removeSong(/* TODO */) {
+
+}
+
+void PendingSongs::reorderSong(/* TODO */) {
+
+}
+
+} // namespace elevation
