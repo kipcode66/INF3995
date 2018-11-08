bc2305beca48d07171b9ecdb69acf74945ef9010
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-11-08 14:46:36 -0500

    Made the move constructor of sqlite_error implicit.

--- project/server/src/common/database/sqlite_error.hpp
+++ project/server/src/common/database/sqlite_error.hpp
@@ -7,11 +7,11 @@ namespace elevation {
 
 class sqlite_error : public std::runtime_error {
 public:
-    explicit sqlite_error(sqlite_error&&) = default;
-    sqlite_error(const sqlite_error&) = default;
     sqlite_error();
     sqlite_error(int);
     sqlite_error(int, const std::string&);
+    sqlite_error(sqlite_error&&) = default;
+    sqlite_error(const sqlite_error&) = default;
     virtual ~sqlite_error();
 
     sqlite_error& operator=(const sqlite_error&) = default;
