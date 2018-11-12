e8c866308c219e302c6f624f9b91e9eb4276181c
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-11-12 12:11:12 -0500

    Created new process for the elevation player.

---
+++ project/server/src/elevation-player/FilesToCompile.cmake
@@ -0,0 +1,26 @@
+# This CMake script contains the files that will be compiled by this
+# sub-project. We do not use file(GLOB_RECURSE ...) because we would need to
+# rerun CMake when we pull from Git and somebody added a file.
+
+include("${CMAKE_SCRIPTS_DIR}/FilesToCompileFunctions.cmake")
+
+###############################################################################
+#                            C++ TRANSLATION UNITS                            #
+###############################################################################
+
+# All .cpp, .cc, .c files EXCEPT main.cpp
+# Use quotes "" if a file contains a space (although not recommended)
+set(ELEVATION_PLAYER_CPP_SOURCES
+)
+
+###############################################################################
+#                                    HEADER FILES                             #
+###############################################################################
+
+# All .h, .hpp, .hh files
+# Use quotes "" if a file contains a space (although not recommended)
+set(ELEVATION_PLAYER_HEADER_SOURCES
+)
+
+makeAbsolute(ELEVATION_PLAYER_CPP_SOURCES)
+makeAbsolute(ELEVATION_PLAYER_HEADER_SOURCES)
