7d87bd879991a2d4e38b558439ec06668204a656
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-11-12 15:50:47 -0500

    Made main use the ArgsParser.

--- project/server/src/elevation-player/main.cpp
+++ project/server/src/elevation-player/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <algorithm>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include "ArgsParser.hpp"
+
+int main(int argc, char** argv) {
+    std::vector<std::string> args;
+    std::transform(
+        argv,
+        argv + argc,
+        std::back_inserter(args),
+        [](const char* arg) { return std::string(arg); }
+    );
+    elevation::ArgsParser argsParser{args};
     return 0;
 }
