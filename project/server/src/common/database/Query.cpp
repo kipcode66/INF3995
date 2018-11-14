35f8ff7017c9dbf8c2269dc4595d5fda13be47e0
Emir Khaled Belhaddad <emir-khaled.belhaddad@polymtl.ca>
2018-11-14 01:56:30 -0500

    a1011: Implemented class Query

--- project/server/src/common/database/Query.cpp
+++ project/server/src/common/database/Query.cpp
@@ -1,12 +1,32 @@
 #include "Query.hpp"
+#include <cstring>
 
 using namespace elevation;
 
-Query::Query(const Query&) { }
-Query::Query(Query&&) { }
-Query::~Query() { }
+Query::Query(const Query& that) {
+    this->operator=(that);
+}
+Query::Query(Query&& that) {
+    this->operator=(std::move(that));
+}
+Query::~Query() {
+    sqlite3_free(m_query);
+}
 
-Query& Query::operator=(const Query&) { }
-Query& Query::operator=(Query&&) { }
+Query& Query::operator=(const Query& that) {
+    auto n = sqlite3_msize(that.m_query);
+    sqlite3_free(this->m_query);
+    this->m_query = (char*)sqlite3_malloc(n);
+    strncpy(this->m_query, that.m_query, n);
+    return *this;
+}
+Query& Query::operator=(Query&& that) {
+    sqlite3_free(this->m_query);
+    this->m_query = that.m_query;
+    that .m_query = nullptr;
+    return *this;
+}
 
-std::string Query::value() const { }
+std::string Query::value() const {
+    return std::string(m_query);
+}
