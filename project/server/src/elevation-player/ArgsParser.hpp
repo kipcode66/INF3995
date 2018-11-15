f6500da71a6f274386692786cb2183ee575d01c3
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-11-15 10:55:54 -0500

    Added a '-p' option to the elevation-player.

--- project/server/src/elevation-player/ArgsParser.hpp
+++ project/server/src/elevation-player/ArgsParser.hpp
@@ -11,12 +11,14 @@ public:
     explicit ArgsParser(const std::vector<std::string>& args);
 
     std::string getCachePath() const { return m_cachePath; }
+    uint16_t    getPort()      const { return m_port; }
 
 protected:
     void usage_();
 
 protected:
     std::string m_cachePath;
+    uint16_t m_port;
 };
 
 } // namespace elevation
