f012eed3877bb2c165886ae6b87ef59345549dd5
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-11-15 11:18:48 -0500

    Created first test for the ArgsParser in elevation-player.

---
+++ project/server/src/elevation-player/test/TestElevationPlayerArgsParser.cpp
@@ -0,0 +1,12 @@
+#define BOOST_TEST_MODULE ArgsParser
+#include <boost/test/unit_test.hpp>
+
+#include <ArgsParser.hpp>
+
+namespace elevation {
+
+BOOST_AUTO_TEST_CASE(constructionDestruction) {
+    
+}
+
+} // namespace elevation
