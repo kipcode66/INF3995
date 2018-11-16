9bb6878bb553c04479911428303926fc87243284
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-11-16 09:46:51 -0500

    Created class EventManager.

---
+++ project/server/src/elevation-player/event/EventManager.cpp
@@ -0,0 +1,8 @@
+#include "EventManager.hpp"
+
+namespace elevation {
+
+EventManager::EventManager() { }
+EventManager::~EventManager() { }
+
+} // namespace elevation
---
+++ project/server/src/elevation-player/event/EventManager.hpp
@@ -0,0 +1,14 @@
+#ifndef EVENT_EVENTMANAGER_HPP
+#define EVENT_EVENTMANAGER_HPP
+
+namespace elevation {
+
+class EventManager {
+public:
+    explicit EventManager();
+    virtual ~EventManager();
+};
+
+} // namespace elevation
+
+#endif // !EVENT_EVENTMANAGER_HPP
