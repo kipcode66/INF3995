d18ee716792a5dfcc6b4c3f00b3a8adf0676082e
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-11-16 11:54:09 -0500

    Added a note about deadlock in the destructor of EventManager.

--- project/server/src/elevation-player/event/EventManager.cpp
+++ project/server/src/elevation-player/event/EventManager.cpp
@@ -12,7 +12,11 @@ EventManager::EventManager(uint16_t port)
     m_accepterThread = std::thread(std::bind(&EventManager::accepterThread_, std::move(listener)));
 }
 
-EventManager::~EventManager() { }
+EventManager::~EventManager() {
+    // NOTE: In theory, since the Elevation Player never finishes until we kill it,
+    // This destructor should never get called. However, if it is, it will block forever.
+    m_accepterThread.join();
+}
 
 void EventManager::accepterThread_(Mp3EventListenerSocket listener) {
     // TODO
