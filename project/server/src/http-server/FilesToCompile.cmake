b7afd847d71b4f1941debed69ee61d888df5fc3d
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-11-19 10:37:20 -0500

    Created class MissingTokenException.

--- project/server/src/http-server/FilesToCompile.cmake
+++ project/server/src/http-server/FilesToCompile.cmake
@@ -11,6 +11,7 @@ include("${CMAKE_SCRIPTS_DIR}/FilesToCompileFunctions.cmake")
 # All .cpp, .cc, .c files EXCEPT main.cpp
 # Use quotes "" if a file contains a space (although not recommended)
 set(HTTP_SERVER_CPP_SOURCES
+    http/exception/MissingTokenException.cpp
     http/RestApi.cpp
 )
 
@@ -21,6 +22,7 @@ set(HTTP_SERVER_CPP_SOURCES
 # All .h, .hpp, .hh files
 # Use quotes "" if a file contains a space (although not recommended)
 set(HTTP_SERVER_HEADER_SOURCES
+    http/exception/MissingTokenException.hpp
     http/RestApi.hpp
 )
 
