1ea3c50915d7967c366bf9e85593d5762599e138
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-11-19 10:47:26 -0500

    Created class InvalidTokenException.

--- project/server/src/http-server/FilesToCompile.cmake
+++ project/server/src/http-server/FilesToCompile.cmake
@@ -12,6 +12,7 @@ include("${CMAKE_SCRIPTS_DIR}/FilesToCompileFunctions.cmake")
 # Use quotes "" if a file contains a space (although not recommended)
 set(HTTP_SERVER_CPP_SOURCES
     http/exception/MissingTokenException.cpp
+    http/exception/InvalidTokenException.cpp
     http/RestApi.cpp
 )
 
@@ -23,6 +24,7 @@ set(HTTP_SERVER_CPP_SOURCES
 # Use quotes "" if a file contains a space (although not recommended)
 set(HTTP_SERVER_HEADER_SOURCES
     http/exception/MissingTokenException.hpp
+    http/exception/InvalidTokenException.hpp
     http/RestApi.hpp
 )
 
