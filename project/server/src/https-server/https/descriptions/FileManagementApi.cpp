208578e860b1e974fbce06b43b708ee1e93fc389
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-11-20 14:50:33 -0500

    Implemented the getSuperviseurFile endpoint.

--- project/server/src/https-server/https/descriptions/FileManagementApi.cpp
+++ project/server/src/https-server/https/descriptions/FileManagementApi.cpp
@@ -1,4 +1,8 @@
 #include "FileManagementApi.hpp"
+#include "Admin.hpp"
+
+#include <common/database/Database.hpp>
+#include <common/rest/rest_utils.hpp>
 
 using namespace Pistache;
 
@@ -24,10 +28,32 @@ FileManagementApi::FileManagementApi(Rest::Description& desc, Logger& logger)
 
 void FileManagementApi::getSuperviseurFile_(const Rest::Request& request,
                                             Http::ResponseWriter response) {
-    response.send(Http::Code::Ok, "getSuperviseurFile called");
-    std::ostringstream logMsg;
-    logMsg << "getSuperviseurFile called ";
-    m_logger.log(logMsg.str());
+    // querying a param from the request object, by name
+    std::thread([this](const Pistache::Rest::Request& request, Pistache::Http::ResponseWriter response) {
+    try {
+        Admin requestAdmin{request};
+        Database* db = Database::instance();
+        if (db->isAdminConnected(requestAdmin.getId())) {
+            std::string serializedList = rest_utils::generateAllSongsAsViewedBy_(requestAdmin.getId(), true);
+
+            std::ostringstream logMsg;
+            logMsg << "The file list for admin \"" << requestAdmin.getId() << "\" was successfuly sent.";
+            m_logger.log(logMsg.str());
+            response.send(Pistache::Http::Code::Ok, serializedList);
+        }
+        else {
+            std::ostringstream errorMsg;
+            errorMsg << "User \"" << requestAdmin.getId() << "\" is not an admin.";
+            m_logger.err("getSuperviseurFile failed: " + errorMsg.str());
+            response.send(Pistache::Http::Code::Forbidden, errorMsg.str());
+        }
+    }
+    catch (const std::exception& e) {
+        m_logger.err(std::string{"getSuperviseurFile failed: "} + e.what());
+        response.send(Pistache::Http::Code::Forbidden, e.what());
+        return;
+    }
+}, std::move(request), std::move(response)).detach();
 }
 
 void FileManagementApi::deleteSuperviseurChanson_(const Rest::Request& request,
