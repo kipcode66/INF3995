b1c9634466e3471232c6042139da520603e387ca
Emir Khaled Belhaddad <emir-khaled.belhaddad@polymtl.ca>
2018-11-22 13:23:06 -0500

    f1163: First implementation done.

--- project/server/src/elevation-player/mp3/Mp3AutoPlayerCallbacks.cpp
+++ project/server/src/elevation-player/mp3/Mp3AutoPlayerCallbacks.cpp
@@ -52,24 +52,32 @@ fs::path Mp3AutoPlayerCallbacks::newSongProvider_() const {
             }
         }
     } while (retry);
-    try {
-        if (newSong != Mp3AutoPlayer::NO_SONG) {
-            Database* db = Database::instance();
-            auto song = db->getSongByPath(newSong.c_str());
-            db->removeSong(song.id);
-        }
-    }
-    catch (sqlite_error& e) {
-        m_logger.err(e.what());
-    }
-    return newSong;
 }
 
 void Mp3AutoPlayerCallbacks::songRemover_(fs::path pathOfSong) {
-    try {
-        m_cache.deleteFile(pathOfSong.filename());
-    }
-    catch (std::experimental::filesystem::filesystem_error& e) {
-        m_logger.err(e.what());
-    }
+    bool retry;
+    do {
+        retry = false;
+        try {
+            Database* db = Database::instance();
+            Song_t oldSong = db->getSongByPath(pathOfSong)
+            if (oldSong.id != 0) {
+                db->removeSong(oldSong.id);
+            }
+            m_cache.deleteFile(pathOfSong.filename());
+        }
+        catch (sqlite_error& e) {
+            if (e.code() == SQLITE_LOCKED ||
+                e.code() == SQLITE_BUSY) {
+                retry = true;
+                std::this_thread::sleep_for(100ms);
+            }
+            else {
+                m_logger.err(e.what());
+            }
+        }
+        catch (fs::filesystem_error& e) {
+            m_logger.err(e.what());
+        }
+    } while (retry);
 }
